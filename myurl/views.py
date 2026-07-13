46078849f6f7cc0975bd96e3b047e73e19004015
Poom <poommuse29@gmail.com>
2026-07-13 11:49:58 +0700

    refactor: use template inheritance, url names, and block content

--- myurl/views.py
+++ myurl/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, "index.html")
     
 def about (request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "about.html")
 
 def contact (request):
-    return HttpResponse("<h1>Contact</h1>")
+    return render(request, "contact.html")
